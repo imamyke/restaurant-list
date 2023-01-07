e0b1c56ac7a00b5280e36d6281bf5ed850b35e60
Amy Ke <xiahamy@gmail.com>
2023-01-07 23:04:35 +0800

    feat: modify index.hbs,style.css,combine sort&search

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -9,19 +9,9 @@ router.get('/', (req, res) => {
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.error(error))
 })
-// 搜尋
+// 搜尋&排序
 router.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim().toLowerCase() || ''
-  Restaurant.find()
-  .lean()
-  .then(restaurant => {
-    const restaurants = restaurant.filter(i => i.name.includes(keyword) || i.category.includes(keyword))
-    return res.render('index', { restaurants, keyword })
-  })
-  .catch(error => console.error(error))
-})
-// 排序
-router.get('/sort', (req, res) => {
   const sort = req.query.sort
   let sorter = {}
   const selected = {
@@ -46,6 +36,18 @@ router.get('/sort', (req, res) => {
     sorter = { location: 'asc' }
     selected.location = true
   } 
+  Restaurant.find()
+  .lean()
+  .sort(sorter)
+  .then(restaurant => {
+    const restaurants = restaurant.filter(i => i.name.includes(keyword) || i.category.includes(keyword))
+    return res.render('index', { restaurants, keyword, selected })
+  })
+  .catch(error => console.error(error))
+})
+// 排序
+router.get('/sort', (req, res) => {
+  
   Restaurant.find()
   .lean()
   .sort(sorter)
