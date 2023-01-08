29671bb4e22544297b0d16b2fb3758cf7095a7d8
Amy Ke <xiahamy@gmail.com>
2023-01-08 16:28:24 +0800

    feat: check search all

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,7 +1,7 @@
 const searchForm = document.querySelector('#search-form')
 searchForm.addEventListener('submit', function onFormSubmitted(event) {
   const searchInput = document.querySelector('.search-input')
-  const keyword = searchInput.value.trim()
+  let keyword = searchInput.value.trim()
   const all = document.querySelector("option[value='all']")
   if (all.selected) {
     keyword = 'all'
