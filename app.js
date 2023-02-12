c71bf0b330da6f310f16faa01ae9f71f5e47da05
Amy Ke <xiahamy@gmail.com>
2023-02-12 21:19:52 +0800

    feat: add facebook login & dotenv

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 // 伺服器設定
 const express = require('express')
 const app = express()
-const port = 3000
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const port = process.env.PORT
 
 // 載入 Session
 const session = require('express-session')
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
