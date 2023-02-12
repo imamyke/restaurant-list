95694304725ffcc50a05065fb7ff611abb99a348
Amy Ke <xiahamy@gmail.com>
2023-02-12 17:43:06 +0800

    feat: add user model

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router()
 // 載入路由分類
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
+router.use('/users', users)
 
 module.exports = router
